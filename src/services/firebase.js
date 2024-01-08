2e30f2c948ed153d3d23d0b8e0ee1052f88e001d
Gautam-Sharma-leewayhertz <95333485+Gautam-Sharma-leewayhertz@users.noreply.github.com>
2024-01-08 11:41:09 +0530

    Update Firebase configuration with environment variables.

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -6,14 +6,15 @@ import { getStorage } from "firebase/storage";
 
 export default class FireBase {
   firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+    apiKey: import.meta.env.VITE_APP_API_KEY,
+    authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_APP_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_APP_MSG_SENDER_ID,
+    appId: import.meta.env.VITE_APP_APP_ID,
+    measurementId: import.meta.env.VITE_APP_MEASUREMENT_ID
   };
+
   constructor() {
     // Initialize Firebase
     this.app = initializeApp(this.firebaseConfig);
